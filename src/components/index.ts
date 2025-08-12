630ed30038179caf14f5993bad96799e02b340ba
Venom 🕷️ <44910620+88qji@users.noreply.github.com>
2025-08-12 01:08:38 -0600

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,21 @@
+#!/usr/bin/env node
+import clear from 'clear';
+import chalk from 'chalk';
 
+import Menu from "./lib/menu.js";
+import CliWallet from './lib/wallet.js';
+import CliConnection from "./lib/connection.js";
+import SetupWallet from "./lib/inq/walletPath.js";
+import SetupCluster from "./lib/inq/cluster.js";
+import yargs from 'yargs';
+import {hideBin} from 'yargs/helpers'
+import {parseLedgerWallet} from "@marinade.finance/ledger-utils";
+
+const VERSION = "2.1.3";
+
+const argv = yargs(hideBin(process.argv)).options({
+    cluster: { type: 'string'},
+    programId: { type: 'string'},
+    programManagerId: { type: 'string'},
+    txMetaProgramId: { type: 'string'},
+  }).parseSync();
