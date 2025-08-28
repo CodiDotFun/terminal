9f1ba0a4993c57daea14bdbfba7d817389f71b27
Mik00 <155748950+mikooski@users.noreply.github.com>
2025-08-28 15:59:18 -0600

    Update ecosystem-feedback.js

--- js/ecosystem-feedback.js
+++ js/ecosystem-feedback.js
@@ -1,1 +1,29 @@
-
+$(document).ready(function () {
+    var splideNews = new Splide( '.splide-feedback', {
+        type   : 'loop',
+        pagination: false,
+        perMove: 1,
+        perPage: 6,
+        autoWidth: true,
+        autoScroll: {
+            speed: 1.5,
+        },
+        breakpoints: {
+            767: {
+                perPage: 1,
+                perMove: 1,
+                autoWidth: true,
+                autoScroll: {
+                    speed: 1,
+                },
+            }
+        },
+        classes: {
+            arrows: 'control-panel-btn',
+            arrow : 'splide__arrows',
+            prev  : 'splide__arrow--prev',
+            next  : 'splide__arrow--next',
+        },
+    });
+    splideNews.mount( window.splide.Extensions );
+});
