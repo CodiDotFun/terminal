c6b910cee68a4b99cad13632820d84709c3706a1
Mik00 <155748950+mikooski@users.noreply.github.com>
2025-08-28 16:00:40 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+$(document).ready(function () {
+
+    // toggle menu
+    $('.section-header .btn-toggle_menu').click(function (data) {
+        var menuList = []
+        $('.section-header .header-navigation__list li').each(function () {
+            var menuItem = $(this).find('a').text();
+            var menuItemAttr = $(this).find('a').attr('href');
+            menuList.push({name: menuItem, attr: menuItemAttr});
+        });
 
