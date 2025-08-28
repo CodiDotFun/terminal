7ef5a1f572414230bab2997fa9d5a2bd0a253837
Mik00 <155748950+mikooski@users.noreply.github.com>
2025-08-28 16:00:47 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,3 +9,11 @@ $(document).ready(function () {
             menuList.push({name: menuItem, attr: menuItemAttr});
         });
 
+   var html = `
+        <div class="mobile-navigation">
+            <div class="container-row">
+                <nav class="mobile-navigation-container">
+                    <ul class="mobile-navigation__list">`;
+                    for (var i in menuList) {
+                        html += `<li class="mobile-navigation__item"><a href="`+menuList[i].attr+`" class="mobile-navigation__link">`+menuList[i].name+`</a></li>`
+                    }
